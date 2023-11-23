3b17c689ad3dd02c17dbde3c8399b5a0e97913da
shania873 <vanaeca@hotmail.com>
2023-11-23 22:32:01 +0100

    basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={"/SensNonCritique-App"}>
       <StrictMode>
         <App />
       </StrictMode>
